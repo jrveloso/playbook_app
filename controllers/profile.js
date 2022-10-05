3335e281e4826f3895e9477ff5b1fa4620fea6b1
Jordan Veloso <jrveloso96@gmail.com>
2022-10-05 17:42:30 -0500

    heroku deploy debugging.

--- controllers/profile.js
+++ controllers/profile.js
@@ -6,11 +6,11 @@ const User = require("../models/User");
 module.exports = {
   getProfile: async (req, res) => {
     try {
-      const response = await fetch('https://api.sportradar.us/nba/trial/v7/en/seasons/2021/REG/standings.json?api_key=nvw29fxe8j7t27fhcu2n7sj5');
-      const data = await response.json();
+      // const response = await fetch('https://api.sportradar.us/nba/trial/v7/en/seasons/2021/REG/standings.json?api_key=nvw29fxe8j7t27fhcu2n7sj5');
+      // const data = await response.json();
 
-      const eastTeams = await data.conferences[1].divisions.map( con => con.teams).flat();
-      const westTeams = await data.conferences[0].divisions.map( con => con.teams).flat()
+      // const eastTeams = await data.conferences[1].divisions.map( con => con.teams).flat();
+      // const westTeams = await data.conferences[0].divisions.map( con => con.teams).flat()
       // console.log(eastTeams, westTeams);
 
       const players = await Player.find({ user: req.params.id })
@@ -20,7 +20,7 @@ module.exports = {
       console.log(posts)
       
 
-      res.render("profile.ejs", { user: req.user, paramsID: req.params.id, players: players, posts: posts, eastTeams: eastTeams, westTeams: westTeams });
+      res.render("profile.ejs", { user: req.user, paramsID: req.params.id, players: players, posts: posts});
     } catch (err) {
       console.log(err);
     }
