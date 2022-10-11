28d2e98434ce23318f0be3cba3cb5df6a87a9301
Jordan Veloso <jrveloso96@gmail.com>
2022-10-11 11:35:53 -0500

    Added hover menu to Teams.

--- controllers/post.js
+++ controllers/post.js
@@ -17,7 +17,6 @@ module.exports = {
       
       //Get todays date
       const today = new Date()
-      // console.log(today.valueOf())
       const year = today.getFullYear()
       const todaysDate = `${today.getFullYear()}${today.getMonth() + 1}${today.getDate().toString().length === 1 ? "0" + today.getDate() : today.getDate()}`
       // console.log(todaysDate)
@@ -26,7 +25,7 @@ module.exports = {
       const results = await fetch(`http://data.nba.net/data/10s/prod/v1/${year}/teams.json`)
       const teamData = await results.json()
       const teams = await teamData.league.standard.filter(team => team.isNBAFranchise === true)
-      // console.log(teamData.league.standard);
+      console.log(teams);
 
       //Players in user's watchlist
       const players = await Player.find({ user: req.user._id })
@@ -35,12 +34,9 @@ module.exports = {
       //Games today
       const response = await fetch(`http://data.nba.net/data/10s/prod/v1/${year}/schedule.json`)
       const scheduleData = await response.json()
-      console.log(scheduleData.league.standard)
+      // console.log(scheduleData.league.standard)
       const todaysGames = scheduleData.league.standard.filter(games => games.startDateEastern === todaysDate)
       // console.log(todaysGames)
-      // console.log(req.user.id)
-      // console.log(posts[0].userId)
-
 
       res.render("feed.ejs", { posts: posts, users: users, user: req.user, players: players, teams: teams, games: todaysGames, time: today});
     } catch (err) {
