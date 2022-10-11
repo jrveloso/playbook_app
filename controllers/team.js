385447f1b2cd3ec3121dc1710fb999dbb6370721
Jordan Veloso <jrveloso96@gmail.com>
2022-10-11 01:31:04 -0500

    debug logos in schedule. added new menu to feed.

--- controllers/team.js
+++ controllers/team.js
@@ -14,7 +14,7 @@ module.exports = {
         const results = await fetch('http://data.nba.net/data/10s/prod/v1/2021/teams.json')
         const teamResults = await results.json()
         const chosenTeam = await teamResults.league.standard.find(team => team.urlName === teamName)
-        // console.log(chosenTeam)
+        console.log(chosenTeam)
 
         const response = await fetch(`http://data.nba.net/data/10s/prod/v1/2022/teams/${teamName}/roster.json`);
         const teamData = await response.json();
