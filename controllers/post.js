74017c584cc8439761b98d4b2575853568a243af
Jordan Veloso <jrveloso96@gmail.com>
2022-10-11 00:01:03 -0500

    Added today's games section and added links to component left of feed.

--- controllers/post.js
+++ controllers/post.js
@@ -8,22 +8,41 @@ const Player = require("../models/Player");
 module.exports = {
   getFeed: async (req, res) => {
     try {
+      //Posts in feed
       const posts = await Post.find().sort({ createdAt: "desc" }).lean();
       
+      //Usernames for posts
       const users = await User.find()
       // console.log(users)
+      
+      //Get todays date
+      const today = new Date()
+      // console.log(today.valueOf())
+      const year = today.getFullYear()
+      const todaysDate = `${today.getFullYear()}${today.getMonth() + 1}${today.getDate().toString().length === 1 ? "0" + today.getDate() : today.getDate()}`
+      // console.log(todaysDate)
 
-      const response = await fetch('https://api.sportradar.us/nba/trial/v7/en/seasons/2021/REG/standings.json?api_key=nvw29fxe8j7t27fhcu2n7sj5');
-      const data = await response.json();
+      //Teams
+      const results = await fetch(`http://data.nba.net/data/10s/prod/v1/${year}/teams.json`)
+      const teamData = await results.json()
+      const teams = await teamData.league.standard.filter(team => team.isNBAFranchise === true)
+      // console.log(teamData.league.standard);
 
-      const eastTeams = await data.conferences[1].divisions.map( con => con.teams).flat();
-      const westTeams = await data.conferences[0].divisions.map( con => con.teams).flat()
-      // console.log(eastTeams, westTeams);
-
+      //Players in user's watchlist
       const players = await Player.find({ user: req.user._id })
       // console.log(players)
 
-      res.render("feed.ejs", { posts: posts, users: users, user: req.user, players: players, eastTeams: eastTeams, westTeams: westTeams});
+      //Games today
+      const response = await fetch(`http://data.nba.net/data/10s/prod/v1/${year}/schedule.json`)
+      const scheduleData = await response.json()
+      console.log(scheduleData.league.standard)
+      const todaysGames = scheduleData.league.standard.filter(games => games.startDateEastern === todaysDate)
+      // console.log(todaysGames)
+      // console.log(req.user.id)
+      // console.log(posts[0].userId)
+
+
+      res.render("feed.ejs", { posts: posts, users: users, user: req.user, players: players, teams: teams, games: todaysGames, time: today});
     } catch (err) {
       console.log(err);
     }
