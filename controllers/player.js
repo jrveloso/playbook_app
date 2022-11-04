45efe34f23a63ba73c0834854a5e7b2d754263ae
Jordan Veloso <jrveloso96@gmail.com>
2022-11-04 02:19:43 -0500

    Debug player add.

--- controllers/player.js
+++ controllers/player.js
@@ -145,7 +145,7 @@ module.exports = {
                 user: req.user.id
             });
             console.log("Player has been added!");
-            res.redirect(`/feed`);
+            res.redirect(`/player/${playerId}`);
         } catch (err) {
             console.log(err);
         }
